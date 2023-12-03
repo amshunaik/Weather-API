afa3694da71c2f4ffc746db1a285cd52378c4a4d
Blew_Dreamer <103670494+amshunaik@users.noreply.github.com>
2023-12-03 16:33:43 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+//import Theme from './SCASS/Theme';
+//import reportWebVitals from './reportWebVitals';
+//import { Provider } from 'react-redux';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+//import Overlays from './component/UI/Modal'
+root.render(
+    <React.StrictMode>
+        
+            <App/>
+        
+       
+
+      
+
+
+
+    </React.StrictMode>
+
+
+
+);
+
+// <Quescover>
+//            <App />
+//        </Quescover>
+
+{/* <Provider store={Theme}> 
+            <App/>
+        </Provider> */}
